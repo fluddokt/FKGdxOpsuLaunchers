2f9e22c771b925fbf62f4946d2254bd91d5d4612
fd <fd@b>
2017-01-22 01:21:00 -0500

    0.14.0a

--- android/src/fluddokt/opsu/android/AndroidLauncher.java
+++ android/src/fluddokt/opsu/android/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package fluddokt.opsu.android;
 
+import fluddokt.ex.DeviceInfo;
 import fluddokt.opsu.fake.GameOpsu;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -13,6 +15,21 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useImmersiveMode = true;
 		config.useWakelock = true;
+		DeviceInfo.info = new DeviceInfo() {
+			@Override
+			public String getInfo() {
+				return 
+						"BOARD: "+Build.BOARD
+						+"\nFINGERPRINT: "+Build.FINGERPRINT
+						+"\nHOST: "+Build.HOST
+						+"\nMODEL: "+Build.MODEL
+						+"\nINCREMENTAL: "+Build.VERSION.INCREMENTAL
+						+"\nRELEASE: "+Build.VERSION.RELEASE
+						
+						;
+				
+			}
+		};
 		initialize(new GameOpsu(), config);
 	}
 }
