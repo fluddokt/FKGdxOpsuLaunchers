08fa79388503f89300b2be685082f695d7104d46
fd <fd@b>
2018-06-11 20:14:19 -0400

    0.16.1a

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.desktop;
 
+import fluddokt.ex.DeviceInfo;
 import fluddokt.opsu.fake.GameOpsu;
 
 import com.badlogic.gdx.Files.FileType;
@@ -7,8 +8,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher {
+	static LwjglApplicationConfiguration config;
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 600;
 		config.addIcon("res/icon16.png", FileType.Internal);
@@ -17,6 +19,15 @@ public class DesktopLauncher {
 		config.foregroundFPS = 240;
 		config.backgroundFPS = 240;
 		//config.audioDeviceBufferCount=240;
+		DeviceInfo.info = new DeviceInfo(){
+
+			@Override
+			public void setFPS(int targetFPS) {
+				config.foregroundFPS = targetFPS;
+			}
+			
+		};
 		new LwjglApplication(new GameOpsu(), config);
+		
 	}
 }
